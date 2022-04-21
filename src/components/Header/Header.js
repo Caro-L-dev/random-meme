bcb3144806326dcdcebc171c3867a893b87f8e64
Caro-L-Dev <liardcar@gmail.com>
2022-04-21 12:28:19 +0200

    chore(components/Header): update jsx

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,10 @@
 import React from "react";
-import header from "../Header/styles/header.scss";
+import headerStyles from "../Header/styles/header.scss";
 
 export default function Header() {
     return (
         <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h2 className="header--title">Random Meme</h2>
         </header>
     )
 };
